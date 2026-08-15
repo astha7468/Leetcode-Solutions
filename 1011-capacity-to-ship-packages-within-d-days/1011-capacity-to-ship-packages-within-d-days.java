c0a7642449d983e85c6cf9031c270711e8455fcf
Astha Rathore <rathoreastha12@gmail.com>
2026-08-15 22:18:06 +0530

    Time: 11 ms (66.45%), Space: 50.4 MB (20.3%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+
+        int low = 0;
+        int high = 0;
+
+        // Find minimum and maximum possible capacity
+        for (int weight : weights) {
+            low = Math.max(low, weight);
+            high += weight;
+        }
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int daysNeeded = 1;
+            int currentWeight = 0;
+
+            for (int weight : weights) {
+
+                if (currentWeight + weight > mid) {
+                   
+                    daysNeeded++;
+                    currentWeight = weight;
+                } else {
+                    currentWeight += weight;
+                }
+            }
+
+            if (daysNeeded <= days) {
+                
+                high = mid - 1;
+            } else {
+               
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
