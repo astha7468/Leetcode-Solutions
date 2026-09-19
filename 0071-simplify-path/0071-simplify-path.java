9d3a662d4bf3a63dce1084b50a6b55ec81fab737
Astha Rathore <rathoreastha12@gmail.com>
2026-09-19 23:38:57 +0530

    Time: 5 ms (51.39%), Space: 44.9 MB (62.18%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] parts = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for (String part : parts) {
+
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        return "/" + String.join("/", stack);
+    }
+}
